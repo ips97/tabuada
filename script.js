e504946964328a1f438e387677aa5dfc56a33c0e
Italo Pinheiro <italo_00pinheiro@hotmail.com>
2022-02-11 18:17:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.lenght == 0){
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value) //transforma em numeros
+        var c = 1
+        tab.innerHTML = ''
+        while (c <=10){
+            var item = document.createElement('option')//cria linha na tag select
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = 'tab${c}'
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+
+}
